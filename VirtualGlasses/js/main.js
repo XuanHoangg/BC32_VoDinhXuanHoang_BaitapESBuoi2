d475e2a15b6b0f482ffa2664364ad1a1ceb1db9e
XuanHoangg <hoangdztv001@gmail.com>
2022-09-07 23:29:03 +0700

    source

--- VirtualGlasses/js/main.js
+++ VirtualGlasses/js/main.js
@@ -9,4 +9,108 @@ let dataGlasses = [
     { id: "G8", src: "./img/g8.jpg", virtualImg: "./img/v8.png", brand: "Polo", name: "NATTY ICE", color: "Red, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim." },
     { id: "G9", src: "./img/g9.jpg", virtualImg: "./img/v9.png", brand: "Coarch", name: "MIDNIGHT VIXEN REMIX", color: "Blue, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet." }
 ];
+let copyGlasses = [];
+function dom(selector) {
+    return document.querySelector(selector)
+}
+function displayGlasses() {
+    let glasses = dataGlasses.reduce((result, glasses) => {
+        return result + `
+            <tr>
+                <td>
+                   <img src="${glasses.src}" data-id ="${glasses.id}" style = "cursor:pointer" width="140px" height="140px" class = "m-2">
+                </td>
+            </tr>
+        `
+    }, "")
+    dom(".vglasses__left").innerHTML = glasses;
+}
+displayGlasses()
+dom(".vglasses__left").addEventListener("click", (evt) => {
+    let glassesId = evt.target.getAttribute("data-id");
+    let index = dataGlasses.findIndex((glasses) => {
+        return glassesId === glasses.id;
+    })
+    if (!glassesId) {
+        return false
+    }
+    else {
+        let avtGlasses = [dataGlasses[index]];
+        copyGlasses = [...avtGlasses];
+        console.log(copyGlasses);
+        displayAvtGlasses(avtGlasses);
+    }
+})
+
+function displayAvtGlasses(avtGlasses) {
+    let glasses = avtGlasses.reduce((result, glasses) => {
+        return result + `
+        <tr>
+            <td>
+                <img src="${glasses.virtualImg}" width="140px" height="140px">
+            </td>
+        </tr>
+        `
+    }, "")
+    dom("#avatar").innerHTML = glasses;
+    let glassesInfo = avtGlasses.reduce((result, glasses) => {
+        return result + `
+            <p>${glasses.name} - ${glasses.brand} (${glasses.color})</p>
+            <p> 
+                <span class="bg-danger">$${glasses.price}</span> 
+                <span class = "text-success">Stocking</span> 
+            </p>
+            <p>${glasses.description}</p>
+        `
+    }, "")
+    dom(".vglasses__info").style.display = "block"
+    dom(".vglasses__info").innerHTML = glassesInfo;
+}
+
+dom("#removeGlasses").addEventListener("click", () => {
+    dom("#avatar").innerHTML = "";
+    dom(".vglasses__info").style.display = "none";
+    dom(".vglasses__info").innerHTML = "";
+})
+
+
+dom("#addGlasses").addEventListener("click", () => {
+    displayAvtGlasses(copyGlasses);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
